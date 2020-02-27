55e6bfa67a94d93d4a27701b2b4b46e3a8d098a1
kowalix007 L.K <kowalix007@gmail.com>
2020-02-27 16:05:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,9 @@ def square_array(array)
   new_array << array.each {|n| new_array << n**2}
   new_array
 end
+
+# def square_array(array)
+#   arr = []
+#   array.each { |i| arr << i ** 2 }
+#   arr
+# end
